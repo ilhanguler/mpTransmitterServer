b69fceab2d06008c67524015834cf991def66b67
İlhan Güler <79455914+ilhanguler@users.noreply.github.com>
2024-10-14 19:51:45 +0300

    Add project files.

---
+++ mpTransmitterServer/mpTransmitterServer.cpp
@@ -0,0 +1,13 @@
+﻿// mpTransmitterServer.cpp : Defines the entry point for the application.
+//
+
+#include "mpTransmitterServer.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+
+    
+}
---
+++ mpTransmitterServer/mpTransmitterServer.h
@@ -0,0 +1,8 @@
+﻿// mpTransmitterServer.h : Include file for standard system include files,
+// or project specific include files.
+
+
+#include <iostream>
+#include <thread>
+
+// TODO: Reference additional headers your program requires here.
