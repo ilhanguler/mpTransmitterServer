5f71e0cf2415b3aa11b43f8686c87210ba097d7c
İlhan Güler <79455914+ilhanguler@users.noreply.github.com>
2024-11-30 19:36:19 +0300

    Comment lines are added

--- mpTransmitterServer/mpTransmitterServer.cpp
+++ mpTransmitterServer/mpTransmitterServer.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "mpTransmitterServer.h"
-#include <regex>
+#include "Windows/HTML_Reader_W.h"
  
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 int main(int argc, char* argv[])
@@ -10,7 +10,13 @@ int main(int argc, char* argv[])
 	std::locale::global(std::locale(".UTF8"));
 
 	greetings();
-
+	dom::element* elem;
+	for (int i = 0; i < 300; i++) {
+		readHtml("C:/Users/ciger/source/repos/mpTransmitterServer/out/build/test/parse3.html", elem);
+		std::cout << "open: " << openedTag << " close: " << closedTag << std::endl;
+		//dom::delTree(elem);
+		elem = nullptr;
+	}
 	HTTPServerApp app;
 	return app.run(argc, argv);
 	
